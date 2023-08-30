4cc3aba83622977b8d36f7956659495da051d7ea
damiano453 <prochaskadamian@gmail.com>
2023-08-30 13:42:46 +0200

    Addon

--- main.py
+++ main.py
@@ -78,35 +78,57 @@ class WebScraper:
     def _get_loss_tree(self, driver):
         #print(self.config.target_urls)
         for iterator, url in enumerate(self.config.target_urls['SPALossTreeLinks']):
-            driver.get(url)
-            time.sleep(2)
-            Timer(2, pyautogui.press, ["esc"]).start()
 
-            Xpath_allUnplannedDowntime = '//*[@id="section_AllUnplannedDT"]/tbody/tr/td[4]/table/tbody/tr/td/input'
-            Xpath_byPoSection = '/html/body/table[3]/tbody/tr[55]/td[2]/b'
-            '/html/body/table[3]/tbody/tr[55]/td[2]/b'
-            element_present = None
-            while element_present == None:
-                time.sleep(2)
-                if str(driver.page_source).lower().find("webdab") != -1:
-                    driver.refresh()
-                elif str(driver.page_source) == "<html><head></head><body></body></html>" or \
-                        str(driver.page_source).find("-DescriptionLT-") != -1:
-                    driver.refresh()
-                else:
-                    print("else")
-                print(element_present)
-                try:
-                    element_present = EC.presence_of_element_located((By.XPATH, Xpath_byPoSection))
-                    WebDriverWait(driver, 10).until(element_present)
-                except TimeoutException:
-                    driver.refresh()
+            # for Packer Reference OPEN+
+            driver = self.__load_spa_page(driver, url)
 
-            self.parser.parse(driver, "crimper")
+            date = self.config.folders_structure['dates'][iterator]
+            shift = self.config.folders_structure['shifts'][iterator]
 
-            time.sleep(2)
+            self.parser.parse_reference_packer(driver, date, shift, self.config.smc_enabled_flag)
+
+            # for Crimper
+            machineLinks = ['SPALossTreeCrimperLinks', 'SPALossTreeCombinerLinks', 'SPALossTreeCatTurnLinks']
+            for machineLink in machineLinks:
+                driver = self.__load_spa_page(driver, self.config.target_urls[machineLinks][iterator])
+
+                self.parser.parse_machines(driver, machineLinks, date, shift, self.config.smc_enabled_flag)
+
+
+
+            time.sleep(5)
+            break
 
             #self._take_screenshot(driver, iterator, "03_Grafana Line Overview.png")
+
+    @staticmethod
+    def __load_spa_page(driver, url):
+        driver.get(url)
+        time.sleep(2)
+        Timer(2, pyautogui.press, ["esc"]).start()
+        Xpath_allUnplannedDowntime = '//*[@id="section_AllUnplannedDT"]/tbody/tr/td[4]/table/tbody/tr/td/input'
+        Xpath_byPoSection = '/html/body/table[3]/tbody/tr[55]/td[2]/b'
+        '/html/body/table[3]/tbody/tr[55]/td[2]/b'
+        element_present = None
+        while element_present == None:
+            print("here")
+            # time.sleep(2)
+            if str(driver.page_source).lower().find("webdab") != -1:
+                driver.refresh()
+            elif str(driver.page_source) == "<html><head></head><body></body></html>" or \
+                    str(driver.page_source).find("-DescriptionLT-") != -1:
+                driver.refresh()
+            else:
+                print("else")
+            print(element_present)
+            try:
+                element_present = EC.presence_of_element_located((By.XPATH, Xpath_byPoSection))
+                WebDriverWait(driver, 10).until(element_present)
+            except TimeoutException:
+                driver.refresh()
+
+        return driver
+
     def _get_grafana(self, driver):
         for iterator, url in enumerate(self.config.target_urls['grafanaLinks']):
             driver.get(url)
@@ -174,42 +196,86 @@ class PageParser:
 
         #print(self.data)
 
-    def parse(self, driver, machine):
+    def parse_reference_packer(self, driver, machine, date, shift, smc_enabled_flag):
         try:
             # Parsing logic using BeautifulSoup or any other HTML parsing library
             # Extract relevant data and return as structured objects
-            #self.data['Date'] = "Sample Title"
-            #self.data['Shift'] = "Sample Description"
-            # Sample data model
             for key in self.data:
-                #if self.parameters[key]["localisation"]["function"] == "extract":
-                Xpath_Row = self.parameters[key]["localisation"]["Xpath_table_raw"]
-                key_word = self.parameters[key]["localisation"]["key_word"]
-                rowsToAdd = self._find_row_in_table(driver, Xpath_Row, key_word)
-                Xpath_Value = self.parameters[key]["localisation"]["Xpath_item"].format(rowsToAdd)
-                print(Xpath_Value)
+                if key == 'CLTC-Number of External Stops [#]':
+                    continue
+                elif key == 'COMB-Number of External Stops [#]':
+                    continue
+                elif key == 'CTU-Number of External Stops [#]':
+                    continue
+
+                function = self.parameters[key]["localisation"]["function"]
+                if function == "assign":
+                    value = self.__assign(key, date, shift, smc_enabled_flag)
+                    self.data[key].append(value)
+                elif function == "extract":
+                    value = self.__extract(driver, key)
+                    self.data[key].append(value)
+                elif function == "extract_plus_1":
+                    value = self.__extract(driver, key, addon=1)
+                    self.data[key].append(value)
+                elif function == "extract_split_number":
+                    value = self.__extract_split_number(driver, key)
+                    self.data[key].append(value)
 
 
 
-                #elif  self.parameters[key]["localisation"]["function"] == "extract_plus_1":
-
-                value = driver.find_element(By.XPATH, Xpath_Value)
-
-                print(value)
-                self.data[key].append(value.text)
-                print(self.data[key])
-
-            #print(self.data)
-            #return self.data
-
+                print({key : self.data[key]})
 
 
         except Exception as e:
             logging.error(f"An error occurred while parsing the page: {e}")
 
+    def parse_machines(self, driver, machineLinks, date, shift, smc_enabled_flag):
+        try:
+            # Parsing logic using BeautifulSoup or any other HTML parsing library
+            # Extract relevant data and return as structured objects
+            for key in self.data:
+                if key == 'CLTC-Number of External Stops [#]' and machineLinks == 'SPALossTreeCrimperLinks':
+                    value = self.__extract(driver, key)
+                    self.data[key].append(value)
+                elif key == 'COMB-Number of External Stops [#]' and machineLinks == 'SPALossTreeCombinerLinks':
+                    value = self.__extract(driver, key)
+                    self.data[key].append(value)
+                elif key == 'CTU-Number of External Stops [#]' and machineLinks == 'SPALossTreeCatTurnLinks':
+                    value = self.__extract(driver, key)
+                    self.data[key].append(value)
+
+        except Exception as e:
+            logging.error(f"An error occurred while parsing the page: {e}")
+
     @staticmethod
-    def extract(self, Xpath_Row, rowsToAdd, Xpath_Value):
-        pass
+    def __assign(key, date, shift, smc_enabled_flag):
+        if key == "Date":
+            return date
+        elif key == "Shift":
+            return shift.rsplit("_")[0]
+        elif key == "w/wo SM":
+            return smc_enabled_flag
+
+    def __extract_split_number(self, driver, key):
+        value = self.__extract(driver, key)
+        for val in value.rsplit(" "):
+            if self.is_float(val):
+                return val
+    def __extract(self, driver, key, addon=0):
+        Xpath_Row = self.parameters[key]["localisation"]["Xpath_table_raw"]
+        key_word = self.parameters[key]["localisation"]["key_word"]
+        try:
+            rowsToAdd = self._find_row_in_table(driver, Xpath_Row, key_word)
+        except:
+            rowsToAdd = 0
+        Xpath_Value = self.parameters[key]["localisation"]["Xpath_item"].format(rowsToAdd+addon)
+        try:
+            value = driver.find_element(By.XPATH, Xpath_Value)
+            return value.text
+        except:
+            value = "NaN"
+            return value
     def _get_parse_parameters(self, file_name):
         self.parameters = self._loadJSON(file_name)
 
@@ -251,7 +317,7 @@ class PageParser:
             return False
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 
     general_parameters = {'LU_information' : "Parameters/LU_information.json",
                   'SHIFT_information' : "Parameters/SHIFT_information.json",
